e5245e005e308bd3873e3a4bd395a978d3074903
nikoavgerinos <avgerinos.niko@gmail.com>
2024-02-01 14:10:26 +0100

    feat(chat-window): First version of chat window using Giftedchat and enhance accessibility on Start Page - Task 5.2

--- components/Chat.js
+++ components/Chat.js
@@ -1,28 +1,74 @@
-import { useEffect } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, View, KeyboardAvoidingView, Platform } from 'react-native';
+import { GiftedChat, Bubble } from "react-native-gifted-chat";
 
 const Chat = ({ route, navigation }) => {
     const { name, background } = route.params;
+    const [messages, setMessages] = useState([]);
+    const onSend = (newMessages) => {
+        setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages))
+    }
+
+    // Customize speech bubble
+    const renderBubble = (props) => {
+        return <Bubble
+            {...props}
+            wrapperStyle={{
+                right: {
+                    backgroundColor: "#757083"
+                },
+                left: {
+                    backgroundColor: "#FFF"
+                }
+            }}
+        />
+    }
 
     useEffect(() => {
         navigation.setOptions({ title: name });
     }, []);
 
+    useEffect(() => {
+        setMessages([
+            {
+                _id: 1,
+                text: "Hello developer",
+                createdAt: new Date(),
+                user: {
+                    _id: 2,
+                    name: "React Native",
+                    avatar: "https://placebear.com/640/360",
+                },
+            },
+            {
+                _id: 2,
+                text: 'This is a system message',
+                createdAt: new Date(),
+                system: true,
+            },
+        ]);
+    }, []);
+
+    {/* style={[styles.container, {backgroundColor: background}]}\*/ }
     return (
         <View style={[styles.container, { backgroundColor: background }]}>
-            <Text>Welcome to the chat</Text>
+            <GiftedChat
+                messages={messages}
+                renderBubble={renderBubble}
+                onSend={messages => onSend(messages)}
+                user={{
+                    _id: 1
+                }}
+            />
+            {Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null}
         </View>
-    );
+    )
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: '100%',
-        height: '100%',
-    }
+    },
 });
 
 export default Chat;
--- components/Start.js
+++ components/Start.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { StyleSheet, View, Text, TouchableOpacity, TextInput, ImageBackground, Image } from 'react-native';
+import { StyleSheet, View, Text, TouchableOpacity, TextInput, ImageBackground, KeyboardAvoidingView, Platform } from 'react-native';
 
 const Start = ({ navigation }) => {
     const [name, setName] = useState('');
@@ -7,36 +7,47 @@ const Start = ({ navigation }) => {
     const colors = ['#090C08', '#474056', '#8A95A5', '#B9C6AE'];
 
     return (
-        <View style={styles.container}>
+        <KeyboardAvoidingView
+            style={styles.container}
+            behavior={Platform.OS === 'ios' ? 'padding' : null}
+        >
             <ImageBackground source={require('../img/BackgroundImage.png')} style={styles.bgImage} resizeMode="cover">
                 <Text style={styles.appTitle}>Chat App</Text>
                 <View style={styles.box}>
-                    {/* Input user name \*/}
-
+                    {/* Input user name */}
                     <TextInput
+                        accessibilityLabel='Username_input'
+                        accessibilityRole='text'
                         style={styles.textInput}
                         value={name}
                         onChangeText={setName}
                         placeholder='Your Name'
                     />
                     <Text style={styles.chooseBackgroundColor}>Choose Background Color</Text>
-                    {/* Choose background color of chat \*/}
+                    {/* Choose background color of chat */}
                     <View style={styles.colorButtonsBox}>
                         {colors.map((color, index) => (
                             <TouchableOpacity
+                                accessibilityLabel='Color Button'
+                                accessibilityHint='Choose a background color for your chat window'
+                                accessibilityRole='button'
                                 key={index}
                                 style={[styles.colorButton, { backgroundColor: color }, background === color && styles.selected]}
                                 onPress={() => setBackground(color)}
                             />
                         ))}
                     </View>
-                    {/* Start Chat \*/}
-                    <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Chat', { name: name, background: background })}>
+                    {/* Start Chat */}
+                    <TouchableOpacity
+                        accessibilityLabel='Start Chatting'
+                        accessibilityRole='button'
+                        style={styles.button}
+                        onPress={() => navigation.navigate('Chat', { name: name, background: background })}>
                         <Text style={styles.buttonText}>Start Chatting</Text>
                     </TouchableOpacity>
                 </View>
             </ImageBackground>
-        </View>
+        </KeyboardAvoidingView>
     );
 }
 
